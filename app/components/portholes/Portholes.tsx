816ede71320fbf154e51884459cbaff8806d6d10
Daler Abdurahimov <01.tolibovich@gmail.com>
2025-01-07 17:54:42 +0500

    autoplay: true

--- app/components/portholes/Portholes.tsx
+++ app/components/portholes/Portholes.tsx
@@ -18,7 +18,7 @@ export const Portholes = () => {
   const settings = {
     dots: false,
     infinite: true,
-    autoplay: false,
+    autoplay: true,
     speed: 500,
     slidesToShow: 7,
     slidesToScroll: 1,
