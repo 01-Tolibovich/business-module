6b76cb5f517c29a299189202076880b2af53e61e
Daler Abdurahimov <01.tolibovich@gmail.com>
2025-02-06 15:02:24 +0500

    animations for partner section

--- app/components/ourPartners/OurPartners.tsx
+++ app/components/ourPartners/OurPartners.tsx
@@ -1,6 +1,10 @@
+"use client"
+
 import Image from "next/image";
-import "./styles.scss";
+import { useAnimationsOnVisible } from "@/hooks";
 import { HeadingUI } from "../ui";
+
+import "./styles.scss";
 
 export const OurPartners = () => {
   const partnersLogo = [
@@ -13,13 +17,15 @@ export const OurPartners = () => {
     "/ourPartners/7.png",
     "/ourPartners/8.png",
   ];
+
+  const {visibleIndexes, refs} = useAnimationsOnVisible()
   return (
     <div className="our-partners">
       <HeadingUI as="h2" textAlign="center">Наши партнеры</HeadingUI>
       <div className="partner-items">
         {partnersLogo.map((img, index) => (
-          <div className="partner-item" key={index}>
-            <Image className="img" src={img} width={200} height={60} alt="img" />
+          <div ref={(el) => {refs.current[index] = el}} className="partner-item" key={index}>
+            <Image className={`img ${visibleIndexes.has(index) ? "is-visible" : ""}`} src={img} width={200} height={60} alt="img" />
           </div>
         ))}
       </div>
