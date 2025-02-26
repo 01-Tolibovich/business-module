609e98d30ba625c7ddd7a7ee8594114f9573442e
Daler Abdurahimov <01.tolibovich@gmail.com>
2025-02-26 14:10:20 +0500

    resolve store error

--- store/searchResult.ts
+++ store/searchResult.ts
@@ -14,6 +14,7 @@ const defaultSearchData: SearchTypes = {
     airport: {}
   },
   message: "",
+  session: ""
 }
 const searchResult = create<searchState>((set) => ({
   searchData: defaultSearchData,
